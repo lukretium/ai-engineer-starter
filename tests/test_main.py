ee9fb705f4668375487fa79fad5f3c88d1136d11
Lukas Hartrumpf <lukas.hartrumpf@nerdware.dev>
2025-04-12 22:01:44 +0200

    fix: test

---
+++ tests/test_main.py
@@ -0,0 +1,9 @@
+from fastapi import status
+from fastapi.testclient import TestClient
+
+
+def test_read_root(client: TestClient):
+    response = client.get("/")
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == {"message": "Welcome to AI Engineer API"}
+    assert response.json() == {"message": "Welcome to AI Engineer API"}
